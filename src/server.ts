d5aa967e526c82715ac6cd86883fd4bca39fe889
IvanAphanasyev <nekaktotak2@gmail.com>
2019-12-04 01:40:29 +0200

    rebuild database connection, try deploy

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import http from "http";
 import app from "./app";
 import { createConnection, Connection } from "typeorm";
-
+import { parse } from "pg-connection-string";
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
@@ -9,22 +9,28 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app.callback());
 
+const url = parse(process.env.DATABASE_URL);
+
 server.on("listening", () => {
   console.log(`http server start listen on port: ${port}, pid: ${process.pid}`);
 });
 server.on("error", err => {
   console.log("server event error, i need handler");
 });
+const isDev = process.env.NODE_ENV == "development";
 
 createConnection({
   type: "postgres",
-  host: "localhost",
-  username: process.env.pg_username,
-  password: process.env.pg_password,
-  database: process.env.pg_database_dev,
+  host: url.host,
+  username: url.user,
+  password: url.password,
+  database: url.database,
   synchronize: true,
   logging: false,
-  entities: ["./src/models/**/*.ts"]
+  entities: [...(isDev ? ["src/models/**/*.ts"] : ["dist/models/**/*.js"])],
+  extra: {
+    ssl: !isDev
+  }
 })
   .then(() => {
     server.listen(port);
